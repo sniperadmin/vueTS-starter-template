10f67551e0a06ea4a32033906eae135dbf51247a
Nasr Galal <nasrbue@gmail.com>
2020-06-22 19:22:00 +0200

    feat: add analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+const BundleAnalyzerPlugin = require('@bundle-analyzer/webpack-plugin')
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  chainWebpack(config) {
+    config.plugin('BundleAnalyzerPlugin')
+      .use(BundleAnalyzerPlugin)
+      .init(Plugin => new Plugin({ token: process.env.BUNDLE_ANALYZER_TOKEN }))
+  }
 }
