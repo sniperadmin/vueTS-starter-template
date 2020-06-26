87897b5830fbf601787cd4b0f300750f4efe0023
Nasr Galal <nasrbue@gmail.com>
2020-06-26 22:05:30 +0200

    feat: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,43 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots:['<rootDir>/tests/unit'],
+  setupFiles: ['<rootDir>/tests/unit/jest.setup.ts'],
+  modulePaths: [
+    '<rootDir>',
+  ],
+  moduleDirectories: [
+    'node_modules',
+  ],
+  globals: {
+    'ts-jest': {
+      babelConfig: './babel.config.js',
+    },
+  },
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'vue'],
+  transform: {
+    // process `*.vue` files with `vue-jest`
+    '.*\\.(vue)$': 'vue-jest',
+    '.*\\.(js)$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
+  },
+  transformIgnorePatterns: ['node_modules'],
+  testRegex: '(/tests/unit/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  testPathIgnorePatterns: [
+    '<rootDir>/tests/unit/jest.setup.ts',
+    // '<rootDir>/tests/unit/mocks',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    // "^~/(.*)$": "<rootDir>/src/$1",
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{vue,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+  ],
+  coveragePathIgnorePatterns: ['node_modules'],
+};
